280670c47f4763bcd41ff2fa61fba73b1be3a9d1
izana <izana@EVA>
2025-03-29 18:02:30 +0100

    V1.0

---
+++ p4_Entornos/src/p4_Entornos/Hola.java
@@ -0,0 +1,11 @@
+package p4_Entornos;
+
+public class Hola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hola me llamo izan");
+	}
+
+}
